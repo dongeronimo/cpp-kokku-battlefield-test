3c5d5b1c95eddc93d075f58b676fc243e174566c
dongeronimo <luciano.geronimo.fnord@gmail.com>
2023-07-14 12:44:21 -0300

    Começando pela habilidade mais simples, a de double Damage

--- SpecialAbility.cpp
+++ SpecialAbility.cpp
@@ -8,7 +8,8 @@ random_device randomDevice;
 std::mt19937 randomNumberGenerator(randomDevice());
 std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
 
-SpecialAbility::SpecialAbility(const float proc) :proc(proc) {
+SpecialAbility::SpecialAbility(const shared_ptr<Character> originator, 
+	const float proc) :proc(proc), originator(originator) {
 
 }
 
@@ -16,4 +17,18 @@ bool SpecialAbility::RollDice()
 {
 	auto dice = distribution(randomNumberGenerator);
 	return dice <= proc;
+}
+
+StrongAttack::StrongAttack(const shared_ptr<Character> originator)
+	:SpecialAbility(originator, STRONG_ATTACK_PROC_CHANCE)
+{
+}
+
+void StrongAttack::Execute(shared_ptr<Character> target)
+{
+	cout << "Player " << originator->PlayerIndex << " will do a strong attack!" << endl;
+	auto oldMultiplier = originator->DamageMultiplier;
+	originator->DamageMultiplier = 2.0f;
+	originator->Attack(target);
+	originator->DamageMultiplier = oldMultiplier;
 }
--- SpecialAbility.h
+++ SpecialAbility.h
@@ -5,14 +5,21 @@ class Battlefield;
 class Character;
 
 using namespace std;
-
+#define STRONG_ATTACK_PROC_CHANCE 0.33f
 class SpecialAbility
 {
 private:
 	const float proc;
+protected:
+	const shared_ptr<Character> originator;
 public:
-	SpecialAbility(const float proc);
+	SpecialAbility(const shared_ptr<Character> originator, const float proc);
 	bool RollDice();
 	virtual void Execute(shared_ptr<Character> target) = 0;
 };
 
+class StrongAttack :public SpecialAbility {
+public:
+	StrongAttack(const shared_ptr<Character> originator);
+	void Execute(shared_ptr<Character> target) override;
+};
